a07da18638b22ccf1552e2fdf17c813f08cdcb76
Tushar <tm2582001@gmail.com>
2023-02-14 22:20:44 +0530

    added word posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ const Home = ({ data }) => {
     <Layout>
       <div>
         <h1>Tushar's Thoughts</h1>
-        <h4>{data.allMarkdownRemark.totalCount}</h4>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
           <BlogLink to={node.fields.slug}>
